c2d45cbf8a772522dd2f38edfce3cb387799602f
Textiili <erkki.tiili@gmail.com>
2024-04-13 14:04:16 +0300

    now getting images work

--- src/main/java/com/opinionet/opinionetservice/WebSecurityConfig.java
+++ src/main/java/com/opinionet/opinionetservice/WebSecurityConfig.java
@@ -9,47 +9,66 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
-    @Autowired
-    private UserDetailsService userDetailsService;
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
       http.authorizeHttpRequests(authorize -> authorize
-      .requestMatchers(antMatcher("/login")).permitAll() //Salli login
-      .requestMatchers(antMatcher("/register")).permitAll() //Salli login
-      .requestMatchers(antMatcher("/css/**")).permitAll() //Salli css
-      .requestMatchers(antMatcher("/images/**")).permitAll() //Salli kuvat
-      .requestMatchers(antMatcher("/")).permitAll() //Salli indeksi
-      .requestMatchers(antMatcher("/reviews/**")).permitAll() //Salli kaikille arvostelut
-      .requestMatchers(antMatcher("/database/**")).permitAll() //salli h2-console
-      .requestMatchers(antMatcher("/api/**")).permitAll() //Salli api
-      .requestMatchers(antMatcher("/error")).permitAll()
-      .anyRequest().authenticated()
+              .requestMatchers(antMatcher("/login")).permitAll() //Salli login
+              .requestMatchers(antMatcher("/register")).permitAll() //Salli login
+              .requestMatchers(antMatcher("/css/**")).permitAll() //Salli css
+              .requestMatchers(antMatcher("/images/**")).permitAll() //Salli kuvat
+              .requestMatchers(antMatcher("/")).permitAll() //Salli indeksi
+              .requestMatchers(antMatcher("/reviews/**")).permitAll() //Salli kaikille arvostelut
+              .requestMatchers(antMatcher("/database/**")).permitAll() //salli h2-console
+              .requestMatchers(antMatcher("/api/**")).permitAll() //Salli api
+              .requestMatchers(antMatcher("/error")).permitAll()
+              .anyRequest().authenticated()
       )
-      .formLogin(formlogin -> formlogin
-            .loginPage("/login")
-            .defaultSuccessUrl("/", true) // <- käytettävyyden kannalta älä ohjaa aina index?
-            .permitAll()
-      )
-      //To enable h2-console
-      .csrf(csrf -> csrf.disable())
-      .headers(headers -> headers.disable())
-      .logout(logout -> logout
-            .invalidateHttpSession(true)
-            .logoutSuccessUrl("/")
-            .permitAll()
-      );
+              .formLogin(formlogin -> formlogin
+                      .loginPage("/login")
+                      .defaultSuccessUrl("/", true) // <- käytettävyyden kannalta älä ohjaa aina index?
+                      .permitAll()
+              )
+              //To enable h2-console
+              .csrf(csrf -> csrf.disable())
+              .headers(headers -> headers.disable())
+              .logout(logout -> logout
+                      .invalidateHttpSession(true)
+                      .logoutSuccessUrl("/")
+                      .permitAll()
+              );
       return http.build();
-    }
+  }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/images/**")
+        .allowedOrigins(
+                "https://cdn2.steamgriddb.com",
+                "https://cdn.cloudflare.steamstatic.com"
+        )
+        .allowedMethods("GET")
+        .allowCredentials(false);
+      }
+    };
+  }
 }
